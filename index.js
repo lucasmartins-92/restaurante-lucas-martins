629ccc607e397e877273f5c73ea457f1a8496fe5
lucasmartins-92 <lucas.mart19@gmail.com>
2026-05-22 20:17:51 -0300

    fix mais erros sonar

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ async function loadDashboardData() {
     try {
         financeMetrics = await computeMonthlyFinance();
     } catch (err) {
-        console.log('Erro ao calcular métricas financeiras:', err && err.message ? err.message : err);
+        console.log('Erro ao calcular métricas financeiras:', err?.message ?? err);
         financeMetrics = {
             totalValueSold: 0,
             numberOfSales: 0,
